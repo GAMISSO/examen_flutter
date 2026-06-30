0b2ef57283290d636521481f05ab3bcae552d516
GAMISSO <koffi-emmanuel.gamisso@ism.edu.sn>
2026-06-30 17:08:47 +0000

    new reposit pour les constants

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,4 +1,4 @@
 class AppConstants {
-  static const String baseUrl = 'http://10.0.2.2:8080';
+  static const String baseUrl = 'http://10.0.2.2:8082';
   static const String phoneKey = 'user_phone';
 }
